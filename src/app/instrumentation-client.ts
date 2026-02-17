f031edc9f8d28e9eb0559f4eea975e1a48ea6c1e
pony <38464007+ma-pony@users.noreply.github.com>
2026-02-17 10:20:17 +0800

    fix: 内联 PostHog init 脚本到 layout head

--- src/app/instrumentation-client.ts
+++
@@ -1,7 +0,0 @@
-import posthog from 'posthog-js';
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-  capture_pageview: false, // We capture manually via PostHogPageView
-  capture_pageleave: true,
-});
